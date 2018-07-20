6ea0d02f2dfbccbacd86c7d68444e28a1eadd346
Sharon Rolel <sharon.rolel@activeallocator.com>
2018-07-20 15:25:42 -0400

    use random nonce

--- webpack/crypto.js
+++ webpack/crypto.js
@@ -20,7 +20,7 @@ const generateToken = (length = 24) =>
 const encrypt = key => async clearText => {
     await new Promise(res => setTimeout(res));
     key = getPbkdf2Hash(key, key, 1, 16);
-    const nonce = Date.now().toString();
+    const nonce = await generateToken(8);
     const encrypted = crypto.bytes_to_string(crypto.AES_GCM.encrypt(
         crypto.string_to_bytes(clearText),
         crypto.string_to_bytes(key),
@@ -31,8 +31,8 @@ const encrypt = key => async clearText => {
 
 const decrypt = key => value => {
     key = getPbkdf2Hash(key, key, 1, 16);
-    const nonce = value.slice(0, 13);
-    const valueArray = new Uint8Array(value.slice(13).split(','));
+    const nonce = value.slice(0, 8);
+    const valueArray = new Uint8Array(value.slice(10).split(','));
     const cipherText = new TextDecoder('utf-8').decode(valueArray);
     return crypto.bytes_to_string(crypto.AES_GCM.decrypt(
         crypto.string_to_bytes(cipherText),
