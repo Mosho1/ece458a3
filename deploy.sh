57a381abcbe5fe8b0d3120f89231771df8b1df4a
Sharon Rolel <sharon.rolel@activeallocator.com>
2018-07-21 03:17:16 -0400

    get password from file

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 set -e
 
 export EMAIL_ACCOUNT="ece458a3@gmail.com"
-export EMAIL_PASSWORD=$(pass Email/ece458a3)
+export EMAIL_PASSWORD=$(cat /tmp/ece458a3)
 
 docker build . --tag a3 -m 500m
 docker run -p 3001:3001 -e EMAIL_ACCOUNT=$EMAIL_ACCOUNT -e EMAIL_PASSWORD=$EMAIL_PASSWORD a3
