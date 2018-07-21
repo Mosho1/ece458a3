8570fb7fa14a6ca97d047614a3643b67ec19ceea
Sharon Rolel <sharon.rolel@activeallocator.com>
2018-07-21 09:54:34 -0400

    remove unused images and containers

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,9 @@ set -e
 export EMAIL_ACCOUNT="ece458a3@gmail.com"
 export EMAIL_PASSWORD=$(cat /tmp/ece458a3)
 
+# remove unused images and containers
+docker ps -aq --no-trunc -f status=exited | xargs docker rm
+docker images -q --filter dangling=true | xargs docker rmi
+
 docker build . --tag a3 -m 500m --rm
 docker run -p 3001:3001 -e EMAIL_ACCOUNT=$EMAIL_ACCOUNT -e EMAIL_PASSWORD=$EMAIL_PASSWORD -e NODE_ENV="production" a3
