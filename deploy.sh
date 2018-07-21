7bfd3c9bd8b0e502b4084c64b528bbd94e22c2c0
Sharon Rolel <sharon.rolel@activeallocator.com>
2018-07-21 05:33:01 -0400

    oops

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ export EMAIL_ACCOUNT="ece458a3@gmail.com"
 export EMAIL_PASSWORD=$(cat /tmp/ece458a3)
 
 docker build . --tag a3 -m 500m
-docker run -p 3001:3001 -e EMAIL_ACCOUNT=$EMAIL_ACCOUNT -e EMAIL_PASSWORD=$EMAIL_PASSWORD a3 -e NODE_ENV="production"
+docker run -p 3001:3001 -e EMAIL_ACCOUNT=$EMAIL_ACCOUNT -e EMAIL_PASSWORD=$EMAIL_PASSWORD -e NODE_ENV="production" a3
