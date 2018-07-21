3964adeb1ba4487c2ac12fbfd1f00cf1ffe2db36
Sharon Rolel <sharon.rolel@activeallocator.com>
2018-07-21 02:44:51 -0400

    fix it again

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ export EMAIL_ACCOUNT="ece458a3@gmail.com"
 export EMAIL_PASSWORD=$(pass Email/ece458a3)
 
 docker build . --tag a3 -m 500m
-docker run -ti -p 3001:3001 a3 -e EMAIL_ACCOUNT=$EMAIL_ACCOUNT -e EMAIL_PASSWORD=$EMAIL_PASSWORD
+docker run -ti -p 3001:3001 -e EMAIL_ACCOUNT=$EMAIL_ACCOUNT -e EMAIL_PASSWORD=$EMAIL_PASSWORD a3
